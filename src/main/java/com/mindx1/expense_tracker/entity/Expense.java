cff25a4e91a16434a65b7423fc974965e11713e4
mindx1 <patelsaksham007@gmail.com>
2026-06-15 16:16:26 +0530

    added expense entity

---
+++ src/main/java/com/mindx1/expense_tracker/entity/Expense.java
@@ -0,0 +1,18 @@
+package com.mindx1.expense_tracker.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double amount;
+    private String category;
+
+    
+}
