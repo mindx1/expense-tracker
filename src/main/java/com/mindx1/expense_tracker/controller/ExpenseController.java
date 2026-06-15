3beaa3ac6e4f5df77fb54e0ee8f37e721b225f04
mindx1 <patelsaksham007@gmail.com>
2026-06-15 23:29:49 +0530

    added controller

---
+++ src/main/java/com/mindx1/expense_tracker/controller/ExpenseController.java
@@ -0,0 +1,31 @@
+package com.mindx1.expense_tracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mindx1.expense_tracker.entity.Expense;
+import com.mindx1.expense_tracker.service.ExpenseService;
+
+@RestController
+@RequestMapping("/expenses")
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @PostMapping
+    public Expense addExpense(@RequestBody Expense expense) {
+        return expenseService.saveExpense(expense);
+    }
+
+    @GetMapping
+    public List<Expense> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+}
